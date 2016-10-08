18414ad8567b4d52eff477a680b8d86bde0c427c
Imanton1 <programstarter@gmail.com>
2016-10-08 13:30:35 -0700

    Delete vidpop.js

--- vidpop.js
+++
@@ -1,4 +0,0 @@
-function vidbox(aim){
-	$('#screen').css({"display": "block", opacity: 0.7, "width":$(document).width(),"height":$(document).height()}); //darken
-	$(aim).css({"display": "block"}).click(function(){$(this).css("display", "none");$('#screen').css("display", "none")}); //show box
-};
