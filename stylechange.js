ce1a77caeb08dbcef89b611cee1999ec0d19d381
Imanton1 <programstarter@gmail.com>
2016-10-08 13:30:37 -0700

    Delete stylechange.js

--- stylechange.js
+++
@@ -1,2 +0,0 @@
-function nighttheme(){document.getElementById('thumb').setAttribute('href', 'nighttheme.css');};
-function daytheme(){document.getElementById('thumb').setAttribute('href', 'daytheme.css');};
